YusDyr/vesta

commit 0831a198b86a4760e83c0eaec78d84bab7098e6c

Fixed several Command Injection.

web/edit/mail/index.php
@@ -70,6 +70,9 @@ if ((!empty($_GET['domain'])) && (!empty($_GET['account'])))  {
     $v_date = $data[$v_account]['DATE'];
     $v_time = $data[$v_account]['TIME'];
 
+    $v_domain = escapeshellarg($_GET['domain']);
+    $v_account = escapeshellarg($_GET['account']);
+
     // Parse autoreply
     if ( $v_autoreply == 'yes' ) {
         exec (VESTA_CMD."v-list-mail-account-autoreply ".$user." '".$v_domain."' '".$v_account."' json", $output, $return_var);
@@ -228,7 +231,7 @@ if ((!empty($_POST['save'])) && (!empty($_GET['domain'])) && (!empty($_GET['acco
         $result = array_diff($valiases, $aliases);
         foreach ($result as $alias) {
             if ((empty($_SESSION['error_msg'])) && (!empty($alias))) {
-                exec (VESTA_CMD."v-delete-mail-account-alias ".$v_username." ".$v_domain." ".$v_account." '".$alias."'", $output, $return_var);
+                exec (VESTA_CMD."v-delete-mail-account-alias ".$v_username." ".$v_domain." ".$v_account." '".escapeshellarg($alias)."'", $output, $return_var);
                 check_return_code($return_var,$output);
                 unset($output);
             }
@@ -254,7 +257,7 @@ if ((!empty($_POST['save'])) && (!empty($_GET['domain'])) && (!empty($_GET['acco
         $result = array_diff($vfwd, $fwd);
         foreach ($result as $forward) {
             if ((empty($_SESSION['error_msg'])) && (!empty($forward))) {
-                exec (VESTA_CMD."v-delete-mail-account-forward ".$v_username." ".$v_domain." ".$v_account." '".$forward."'", $output, $return_var);
+                exec (VESTA_CMD."v-delete-mail-account-forward ".$v_username." ".$v_domain." ".$v_account." '".escapeshellarg($forward)."'", $output, $return_var);
                 check_return_code($return_var,$output);
                 unset($output);
             }

web/edit/db/index.php
@@ -42,6 +42,8 @@ if ( $v_suspended == 'yes' ) {
     $v_status =  'active';
 }
 
+$v_database = escapeshellarg($_GET['database']);
+
 // Check POST request
 if (!empty($_POST['save'])) {
     $v_username = $user;

web/edit/web/index.php
@@ -36,7 +36,7 @@ $v_cgi = $data[$v_domain]['CGI'];
 $v_elog = $data[$v_domain]['ELOG'];
 $v_ssl = $data[$v_domain]['SSL'];
 if (!empty($v_ssl)) {
-    exec (VESTA_CMD."v-list-web-domain-ssl ".$user." '".$v_domain."' json", $output, $return_var);
+    exec (VESTA_CMD."v-list-web-domain-ssl ".$user." '".escapeshellarg($v_domain)."' json", $output, $return_var);
     $ssl_str = json_decode(implode('', $output), true);
     unset($output);
     $v_ssl_crt = $ssl_str[$v_domain]['CRT'];
@@ -129,6 +129,7 @@ if (!empty($_POST['save'])) {
         exec (VESTA_CMD."v-list-dns-domain ".$v_username." ".$v_domain." json", $output, $return_var);
         unset($output);
         if ($return_var == 0 ) {
+            $v_ip = escapeshellarg($_POST['v_ip']);
             exec (VESTA_CMD."v-change-dns-domain-ip ".$v_username." ".$v_domain." ".$v_ip." 'no'", $output, $return_var);
             check_return_code($return_var,$output);
             unset($output);
@@ -142,6 +143,7 @@ if (!empty($_POST['save'])) {
             exec (VESTA_CMD."v-list-dns-domain ".$v_username." '".$v_alias."' json", $output, $return_var);
             unset($output);
             if ($return_var == 0 ) {
+                $v_ip = escapeshellarg($_POST['v_ip']);
                 exec (VESTA_CMD."v-change-dns-domain-ip ".$v_username." '".$v_alias."' ".$v_ip, $output, $return_var);
                 check_return_code($return_var,$output);
                 unset($output);
@@ -367,7 +369,7 @@ if (!empty($_POST['save'])) {
     // Add Lets Encrypt support
     if ((!empty($_POST['v_ssl'])) && ( $v_letsencrypt == 'no' ) && (!empty($_POST['v_letsencrypt'])) && empty($_SESSION['error_msg'])) {
         $l_aliases = str_replace("\n", ',', $v_aliases);
-        exec (VESTA_CMD."v-add-letsencrypt-domain ".$user." ".$v_domain." '".$l_aliases."' 'no'", $output, $return_var);
+        exec (VESTA_CMD."v-add-letsencrypt-domain ".$user." ".$v_domain." '".escapeshellarg($l_aliases)."' 'no'", $output, $return_var);
         check_return_code($return_var,$output);
         unset($output);
         $v_letsencrypt = 'yes';

commit 7bb5d5c97546e1718764523594c07c20b567fb80

Update from rodin/master (#2)
* Release 0.9.8-25

* Translated added string in sr.php

* Changing /usr/local/vesta to $VESTA

* added support for HTTP/2 Let's Encrypt servers

* Release 0.9.8-26

* added month to the release log date

* update id.php

* Update nl.php

* Fixing broken autoreply output

Broken in https://github.com/serghey-rodin/vesta/commit/0831a198b86a4760e83c0eaec78d84bab7098e6c#diff-f07da7937954ae08bdb2e7379655ae29
@sergio-nadal

Co-authored-by: Serghey Rodin <serghey.rodin@gmail.com>
Co-authored-by: dpeca <peca@mycity.rs>
Co-authored-by: Taufik Mulyana <nothinux@gmail.com>
Co-authored-by: Robin Dirksen <robin@famdirksen.nl>
Co-authored-by: Alexandr Loskutov <alex_connor@icloud.com>
Co-authored-by: Anton Reutov <a.reutov@outlook.com>

bin/v-add-firewall-chain
@@ -22,7 +22,7 @@ protocol=$(echo $protocol|tr '[:lower:]' '[:upper:]')
 iptables="/sbin/iptables"
 
 # Get vesta port by reading nginx.conf
-vestaport=$(grep 'listen' /usr/local/vesta/nginx/conf/nginx.conf | awk '{print $2}' | sed "s|;||")
+vestaport=$(grep 'listen' $VESTA/nginx/conf/nginx.conf | awk '{print $2}' | sed "s|;||")
 if [ -z "$vestaport" ]; then
     vestaport=8083
 fi

bin/v-add-letsencrypt-domain
@@ -121,8 +121,8 @@ fi
 
 # Requesting nonce / STEP 1
 answer=$(curl -s -I "$API/directory")
-nonce=$(echo "$answer" |grep Nonce |cut -f2 -d \ |tr -d '\r\n')
-status=$(echo "$answer"|grep HTTP/1.1 |tail -n1 |cut -f 2 -d ' ')
+nonce=$(echo "$answer" |grep -i nonce |cut -f2 -d \ |tr -d '\r\n')
+status=$(echo "$answer"|grep HTTP/ |tail -n1 |cut -f 2 -d ' ')
 if [[ "$status" -ne 200 ]]; then
     check_result $E_CONNECT "Let's Encrypt nonce request status $status"
 fi
@@ -137,10 +137,10 @@ done
 payload=$(echo "$payload"|sed "s/,$//")
 payload=$payload']}'
 answer=$(query_le_v2 "$url" "$payload" "$nonce")
-nonce=$(echo "$answer" |grep Nonce |cut -f2 -d \ |tr -d '\r\n')
+nonce=$(echo "$answer" |grep -i nonce |cut -f2 -d \ |tr -d '\r\n')
 authz=$(echo "$answer" |grep "acme/authz" |cut -f2 -d '"')
 finalize=$(echo "$answer" |grep 'finalize":' |cut -f4 -d '"')
-status=$(echo "$answer" |grep HTTP/1.1 |tail -n1 |cut -f2 -d ' ')
+status=$(echo "$answer" |grep HTTP/ |tail -n1 |cut -f2 -d ' ')
 if [[ "$status" -ne 201 ]]; then
     check_result $E_CONNECT "Let's Encrypt new auth status $status"
 fi
@@ -151,8 +151,8 @@ for auth in $authz; do
     answer=$(query_le_v2 "$auth" "$payload" "$nonce")
     url=$(echo "$answer" |grep -A3 $proto |grep url |cut -f 4 -d \")
     token=$(echo "$answer" |grep -A3 $proto |grep token |cut -f 4 -d \")
-    nonce=$(echo "$answer" |grep Nonce |cut -f2 -d \ |tr -d '\r\n')
-    status=$(echo "$answer"|grep HTTP/1.1 |tail -n1 |cut -f 2 -d ' ')
+    nonce=$(echo "$answer" |grep -i nonce |cut -f2 -d \ |tr -d '\r\n')
+    status=$(echo "$answer"|grep HTTP/ |tail -n1 |cut -f 2 -d ' ')
     if [[ "$status" -ne 200 ]]; then
         check_result $E_CONNECT "Let's Encrypt acme/authz bad status $status"
     fi
@@ -210,8 +210,8 @@ for auth in $authz; do
         payload='{}'
         answer=$(query_le_v2 "$url" "$payload" "$nonce")
         validation=$(echo "$answer"|grep -A1 $proto |tail -n1|cut -f4 -d \")
-        nonce=$(echo "$answer" |grep Nonce |cut -f2 -d \ |tr -d '\r\n')
-        status=$(echo "$answer"|grep HTTP/1.1 |tail -n1 |cut -f 2 -d ' ')
+        nonce=$(echo "$answer" |grep -i nonce |cut -f2 -d \ |tr -d '\r\n')
+        status=$(echo "$answer"|grep HTTP/ |tail -n1 |cut -f 2 -d ' ')
         if [[ "$status" -ne 200 ]]; then
             check_result $E_CONNECT "Let's Encrypt validation status $status"
         fi
@@ -236,8 +236,8 @@ ssl_dir=$($BIN/v-generate-ssl-cert "$domain" "info@$domain" "US" "California"\
 csr=$(openssl req -in $ssl_dir/$domain.csr -outform DER |encode_base64)
 payload='{"csr":"'$csr'"}'
 answer=$(query_le_v2 "$finalize" "$payload" "$nonce")
-nonce=$(echo "$answer" |grep Nonce |cut -f2 -d \ |tr -d '\r\n')
-status=$(echo "$answer"|grep HTTP/1.1 |tail -n1 |cut -f 2 -d ' ')
+nonce=$(echo "$answer" |grep -i nonce |cut -f2 -d \ |tr -d '\r\n')
+status=$(echo "$answer"|grep HTTP/ |tail -n1 |cut -f 2 -d ' ')
 certificate=$(echo "$answer"|grep 'certificate":' |cut -f4 -d '"')
 if [[ "$status" -ne 200 ]]; then
     check_result $E_CONNECT "Let's Encrypt finalize bad status $status"

bin/v-add-letsencrypt-user
@@ -103,16 +103,16 @@ fi
 
 
 # Requesting ACME nonce
-nonce=$(curl -s -I "$API/directory" |grep Nonce |cut -f 2 -d \ |tr -d '\r\n')
+nonce=$(curl -s -I "$API/directory" |grep -i nonce |cut -f2 -d\ |tr -d '\r\n')
 
 # Creating ACME account
 url="$API/acme/new-acct"
 payload='{"termsOfServiceAgreed": true}'
 answer=$(query_le_v2 "$url" "$payload" "$nonce")
-kid=$(echo "$answer" |grep Location: |cut -f2 -d ' '|tr -d '\r')
+kid=$(echo "$answer" |grep -i location: |cut -f2 -d ' '|tr -d '\r')
 
 # Checking answer status
-status=$(echo "$answer" |grep HTTP/1.1 |tail -n1 |cut -f2 -d ' ')
+status=$(echo "$answer" |grep HTTP/ |tail -n1 |cut -f2 -d ' ')
 if [[ "${status:0:2}" -ne "20" ]]; then
     check_result $E_CONNECT "Let's Encrypt acc registration failed $status"
 fi

src/deb/ioncube/control
@@ -1,7 +1,7 @@
 Source: vesta-ioncube
 Package: vesta-ioncube
 Priority: optional
-Version: 0.9.8-24
+Version: 0.9.8-26
 Section: admin
 Maintainer: Serghey Rodin <skid@vestacp.com>
 Homepage: https://www.ioncube.com

src/deb/nginx/control
@@ -1,7 +1,7 @@
 Source: vesta-nginx
 Package: vesta-nginx
 Priority: optional
-Version: 0.9.8-24
+Version: 0.9.8-26
 Section: admin
 Maintainer: Serghey Rodin <skid@vestacp.com>
 Homepage: http://vestacp.com

src/deb/php/control
@@ -1,7 +1,7 @@
 Source: vesta-php
 Package: vesta-php
 Priority: optional
-Version: 0.9.8-24
+Version: 0.9.8-26
 Section: admin
 Maintainer: Serghey Rodin <skid@vestacp.com>
 Homepage: http://vestacp.com

src/deb/softaculous/control
@@ -1,7 +1,7 @@
 Source: vesta-softaculous
 Package: vesta-softaculous
 Priority: optional
-Version: 0.9.8-24
+Version: 0.9.8-26
 Section: admin
 Maintainer: Serghey Rodin <skid@vestacp.com>
 Homepage: https://www.softaculous.com

src/deb/vesta/control
@@ -1,7 +1,7 @@
 Source: vesta
 Package: vesta
 Priority: optional
-Version: 0.9.8-24
+Version: 0.9.8-26
 Section: admin
 Maintainer: Serghey Rodin <skid@vestacp.com>
 Homepage: http://vestacp.com

upd/add_notifications.sh
@@ -5,4 +5,4 @@ rm -f /usr/local/vesta/data/users/admin/notifications.conf
 /usr/local/vesta/bin/v-add-user-notification admin "File Manager" "Browse, copy, edit, view, and retrieve all your web domain files using a fully featured <a href='http://vestacp.com/features/#filemanager'>File Manager</a>. Plugin is available for <a href='/edit/server/?lead=filemanager#module-filemanager'>purchase</a>." 'filemanager'
 /usr/local/vesta/bin/v-add-user-notification admin "Chroot SFTP" "If you want to have SFTP accounts that will be used only to transfer files (and not to SSH), you can  <a href='/edit/server/?lead=sftp#module-sftp'>purchase</a> and enable <a href='http://vestacp.com/features/#sftpchroot'>SFTP Chroot</a>"
 /usr/local/vesta/bin/v-add-user-notification admin "Softaculous" "Softaculous is one of the best Auto Installers and it is finally <a href='/edit/server/?lead=sftp#module-softaculous'>available</a>"
-/usr/local/vesta/bin/v-add-user-notification admin "Release 0.9.8-24" "This release is about stability and refinement. We added Let's Encrypt v2 support and added server certificate management tools. For more information please read <a href='http://vestacp.com/roadmap/#0.9.8-23'>release notes</a>"
+/usr/local/vesta/bin/v-add-user-notification admin "Release 0.9.8-26" "This release adds support for Lets Encrypt HTTP/2. For more information please read <a href='http://vestacp.com/history/#0.9.8-26'>release notes</a>"

web/edit/mail/index.php
@@ -72,13 +72,14 @@ if ((!empty($_GET['domain'])) && (!empty($_GET['account'])))  {
 
     $v_domain = escapeshellarg($_GET['domain']);
     $v_account = escapeshellarg($_GET['account']);
+    $v_account_without_quotas = $_GET['account'];
 
     // Parse autoreply
     if ( $v_autoreply == 'yes' ) {
         exec (VESTA_CMD."v-list-mail-account-autoreply ".$user." ".$v_domain." ".$v_account." json", $output, $return_var);
         $autoreply_str = json_decode(implode('', $output), true);
         unset($output);
-        $v_autoreply_message = $autoreply_str[$v_account]['MSG'];
+        $v_autoreply_message = $autoreply_str[$v_account_without_quotas]['MSG'];
         $v_autoreply_message=str_replace("\\n", "\n", $v_autoreply_message);
     }
 }

web/inc/i18n/id.php
@@ -158,10 +158,10 @@ $LANG['id'] = array(
     'minute'  => 'menit',
     'hour'  => 'jam',
     'day'  => 'hari',
-    'days'  => ' hari',
-    'hours'  => ' jam',
-    'minutes'  => ' menit',
-    'month'  => ' bulan',
+    'days'  => 'hari',
+    'hours'  => 'jam',
+    'minutes'  => 'menit',
+    'month'  => 'bulan',
     'package'  => 'paket',
     'Bandwidth'  => 'Bandwidth',
     'Disk'  => 'Diska',
@@ -196,7 +196,7 @@ $LANG['id'] = array(
     'SSL Home Directory'  => 'Direktori Home SSL',
     'Lets Encrypt Support'  => 'Dukungan Lets Encrypt',
     'Lets Encrypt'  => 'Lets Encrypt',
-    'Your certificate will be automatically issued in 5 minutes' => 'Sertifikat anda akan otomatis didapat dalam 5 menit',
+    'Your certificate will be automatically issued in 5 minutes' => 'Sertifikat anda akan otomatis diterbitkan dalam 5 menit',
     'Proxy Support'  => 'Dukungan Proxy',
     'Proxy Extensions'  => 'Ekstensi Proxy',
     'Web Statistics'  => 'Statistik Web',
@@ -219,11 +219,11 @@ $LANG['id'] = array(
     'IMAP hostname'  => 'hostname IMAP',
     'IMAP port'  => 'port IMAP',
     'IMAP security'  => 'keamanan IMAP',
-    'IMAP auth method'  => 'metode autentikasi IMAP',
+    'IMAP auth method'  => 'metode otentikasi IMAP',
     'SMTP hostname'  => 'hostname SMTP',
     'SMTP port'  => 'port SMTP',
     'SMTP security'  => 'keamanan SMTP',
-    'SMTP auth method'  => 'metode autentikasi SMTP',
+    'SMTP auth method'  => 'metode otentikasi SMTP',
     'STARTTLS'  => 'STARTTLS',
     'Normal password'  => 'kata sandi',
     'database'  => 'basis data',
@@ -237,7 +237,7 @@ $LANG['id'] = array(
     'Day of week'  => 'Hari dalam Minggu',
     'local'  => 'lokal',
     'Run Time'  => 'Run Time',
-    'Backup Size'  => 'Ukuran Backup',
+    'Backup Size'  => 'Ukuran Cadangan',
     'SYS'  => 'SYS',
     'Domains'  => 'Domains',
     'Status'  => 'Status',
@@ -268,25 +268,25 @@ $LANG['id'] = array(
     'ftp server'  => 'server ftp',
     'job scheduler'  => 'jadwal pekerjaan',
     'firewall'  => 'firewall',
-    'brute-force monitor'  => 'brute-force monitor',
+    'brute-force monitor'  => 'pantau brute-force',
     'CPU'  => 'CPU',
     'Memory'  => 'Memori',
     'Uptime'  => 'Uptime',
     'core package'  => 'paket inti',
     'php interpreter'  => 'php interpreter',
-    'internal web server'  => 'internal web server',
+    'internal web server'  => 'server web internal',
     'Version'  => 'Versi',
     'Release'  => 'Rilis',
     'Architecture'  => 'Arsitektur',
     'Object'  => 'Objek',
     'Username'  => 'Nama Pengguna',
     'Password'  => 'Kata sandi',
-    'Email'  => 'Email',
+    'Email'  => 'Surel',
     'Package'  => 'Paket',
     'Language'  => 'Bahasa',
     'First Name'  => 'Nama Depan',
     'Last Name'  => 'Nama Belakang',
-    'Send login credentials to email address'  => 'Kirim kredensial login ke alamat email',
+    'Send login credentials to email address'  => 'Kirim kredensial login ke alamat surel',
     'Default Template'  => 'Template Standar',
     'Default Name Servers'  => 'Name Servers Standar',
     'Domain'  => 'Domain',
@@ -300,13 +300,13 @@ $LANG['id'] = array(
     'SSL Certificate Authority / Intermediate'  => 'Otoritas Sertifikat SSL / Menengah',
     'SSL CSR' => 'SSL CSR',
     'optional'  => 'opsi',
-    'internal' => 'intern',
+    'internal' => 'internal',
     'Statistics Authorization'  => 'Statistik Otoritas',
     'Statistics Auth'  => 'Statistik Otoritas',
     'Account'  => 'Pengguna',
-    'Prefix will be automaticaly added to username'  => 'Prefix %s nantinya ditambahin otomatis ke nama pengguna',
-    'Send FTP credentials to email'  => 'Kirim kredensial FTP ke email',
-    'Expiration Date'  => 'Tanggal Kadaluarsa',
+    'Prefix will be automaticaly added to username'  => 'Prefix %s akan otomatis ditambahkan ke nama pengguna',
+    'Send FTP credentials to email'  => 'Kirim kredensial FTP ke surel',
+    'Expiration Date'  => 'Tanggal Kadaluwarsa',
     'YYYY-MM-DD'  => 'YYYY-MM-DD',
     'Name servers'  => 'Name server',
     'Record'  => 'Record',
@@ -316,7 +316,7 @@ $LANG['id'] = array(
     'in megabytes'  => 'dalam megabytes',
     'Message'  => 'Pesan',
     'use local-part'  => 'gunakan part lokal',
-    'one or more email addresses'  => 'satu atau lebih alamat email',
+    'one or more email addresses'  => 'satu atau lebih alamat surel',
     'Prefix will be automaticaly added to database name and database user'  => 'Prefix  %s akan otomatis ditambahkan ke nama basis data dan pengguna basis data',
     'Database'  => 'Basis Data',
     'Type'  => 'Jenis',
@@ -407,7 +407,7 @@ $LANG['id'] = array(
     'cron job' => 'cron job',
 
     'cron' => 'cron',
-    'user dir' => 'dir pengguna',
+    'user dir' => 'direktori pengguna',
 
     'unlimited'  => 'tidak terbatas',
     '1 account'  => '1 pengguna',
@@ -489,14 +489,14 @@ $LANG['id'] = array(
     'RESTART_CONFIRMATION' => 'Apakah anda yakin ingin me-restart %s?',
     'Welcome'  => 'Selamat Datang',
     'LOGGED_IN_AS'  => 'Masuk sebagai pengguna %s',
-    'Error'  => 'Kesalahan',
+    'Error'  => 'Galat',
     'Invalid username or password'  => 'Nama pengguna atau kata sandi salah',
     'Invalid username or code'  => 'Kode atau nama pengguna salah',
     'Passwords not match'  => 'Kata sandi tidak sama',
     'Please enter valid email address.'  => 'Masukkan alamat surel yang valid.',
     'Field "%s" can not be blank.'  => 'Field "%s" tidak boleh kosong.',
     'Password is too short.'  => 'Kata sandi terlalu pendek (minimal 6 karakter)',
-    'Error code:'  => 'Kode kesalahan: %s',
+    'Error code:'  => 'Kode galat: %s',
     'SERVICE_ACTION_FAILED'  => '"%s" "%s" gagal',
     'IP address is in use' => 'Alamat IP sudah digunakan',
     'BACKUP_SCHEDULED'  => 'Tugas sudah ditambahkan ke antrian. email pemberitahuan akan dikirim ketika cadangan telah siap diunduh.',
@@ -560,11 +560,11 @@ $LANG['id'] = array(
     'MAIL Server' => 'MAIL Server',
     'Antivirus' => 'Antivirus',
     'AntiSpam' => 'AntiSpam',
-    'Use Web Domain SSL Certificate' => 'Use Web Domain SSL Certificate',
+    'Use Web Domain SSL Certificate' => 'Gunakan Sertifikat SSL Domain Web',
     'Webmail URL' => 'Webmail URL',
-    'MySQL Support' => 'MySQL Support',
+    'MySQL Support' => 'Dukungan MySQL',
     'phpMyAdmin URL' => 'phpMyAdmin URL',
-    'PostgreSQL Support' => 'PostgreSQL Support',
+    'PostgreSQL Support' => 'Dukungan PostgreSQL',
     'phpPgAdmin URL' => 'phpPgAdmin URL',
     'Maximum Number Of Databases' => 'Jumlah maximal basis data',
     'Current Number Of Databases' => 'Jumlah database saat ini',
@@ -581,7 +581,7 @@ $LANG['id'] = array(
     'Reseller Role' => 'Aturan Reseller',
     'Web Config Editor' => 'Web Config Editor',
     'Template Manager' => 'Template Manager',
-    'Backup Migration Manager' => 'Backup Migration Manager',
+    'Backup Migration Manager' => 'Manajer Migrasi Cadangan',
     'FileManager' => 'FileManager',
     'show: CPU / MEM / NET / DISK' => 'tampilkan: CPU / MEM / NET / DISK',
 
@@ -598,7 +598,7 @@ $LANG['id'] = array(
     'name' => 'nama',
     'Initializing' => 'Inisialisasi',
     'UPLOAD' => 'UNGGAH',
-    'NEW FILE' => 'FILE BARU',
+    'NEW FILE' => 'BERKAS BARU',
     'NEW DIR' => 'DIREKTORI BARU',
     'DELETE' => 'HAPUS',
     'RENAME' => 'GANTI',
@@ -612,10 +612,10 @@ $LANG['id'] = array(
     'Hit' => 'Hit',
     'to reload the page' => 'untuk memuat ulang halaman',
     'Directory name cannot be empty' => 'Nama Direktori tidak boleh kosong',
-    'File name cannot be empty' => 'Nama File tidak boleh kosong',
-    'No file selected' => 'Tidak ada file yang dipilih',
-    'No file or folder selected' => 'Tidak ada file atau folder yg dipilih',
-    'File type not supported' => 'Tipe file tidak didukung',
+    'File name cannot be empty' => 'Nama berkas tidak boleh kosong',
+    'No file selected' => 'Tidak ada berkas yang dipilih',
+    'No file or folder selected' => 'Tidak ada berkas atau folder yg dipilih',
+    'File type not supported' => 'Tipe berkas tidak didukung',
     'Directory download not available in current version' => 'Direktori Unduh tidak tersedia di versi ini',
     'Directory not available' => 'Direktori tidak tersedia',
     'Done' => 'Selesai',
@@ -633,21 +633,21 @@ $LANG['id'] = array(
     'YOU ARE COPYING' => 'ANDA MENYALIN',
     'YOU ARE REMOVING' => 'ANDA MENGHAPUS',
     'Delete items' => 'Hapus item',
-    'Copy files' => 'Salin file',
-    'Move files' => 'Pindah file',
+    'Copy files' => 'Salin berkas',
+    'Move files' => 'Pindah berkas',
     'Are you sure you want to copy' => 'Apakah anda yakin ingin menyalin',
     'Are you sure you want to move' => 'Apakah anda yakin ingin memindahkan',
     'Are you sure you want to delete' => 'Aapakah anda Yakin ingin menghapus',
     'into' => 'ke',
-    'existing files will be replaced' => 'file yang sudah ada akan diganti',
+    'existing files will be replaced' => 'berkas yang sudah ada akan diganti',
     'Original name' => 'Nama orisinal',
-    'File' => 'File',
+    'File' => 'Berkas',
     'already exists' => 'sudah ada',
-    'Create file' => 'Buat file',
+    'Create file' => 'Buat berkas',
     'Create directory' => 'Buat direktori',
-    'read by owner' => 'dibaca oleh owner',
-    'write by owner' => 'ditulis oleh owner',
-    'execute/search by owner' => 'eksekusi/cari oleh owner',
+    'read by owner' => 'dibaca oleh pemilik',
+    'write by owner' => 'ditulis oleh pemilik',
+    'execute/search by owner' => 'eksekusi/cari oleh pemilik',
     'read by group' => 'dibaca oleh grup',
     'write by group' => 'ditulis oleh grup',
     'execute/search by group' => 'ekseskusi/cari oleh grup',
@@ -671,30 +671,30 @@ $LANG['id'] = array(
     'Move backward through top menu' => 'Pindah ke sebelumnya lewat menu atas',
     'Move forward through top menu' => 'Pindah ke setelahnya lewat menu atas',
     'Enter focused element' => 'Masuk ke fokus elemen',
-    'Move up through elements list' => 'Move up through elements list',
-    'Move down through elements list' => 'Move down through elements list',
+    'Move up through elements list' => 'Pindah ke atas melalui daftar elemen',
+    'Move down through elements list' => 'Pindah ke bawah melalui daftar elemen',
 
     'Upload' => 'Unggah',
-    'New File' => 'File baru',
+    'New File' => 'Berkas baru',
     'New Folder' => 'Folder baru',
     'Download' => 'Unduh',
     'Archive' => 'Arsip',
-    'Save File (in text editor)' => 'Simpan File (di text editor)',
+    'Save File (in text editor)' => 'Simpan berkas (di penyunting teks)',
     'Close Popup / Cancel' => 'Tutup Popup / Batal',
     'Move Cursor Up' => 'Pindahkan kursor keatas',
     'Move Cursor Down' => 'Pindahkan kursor kebawah',
     'Switch to Left Tab' => 'Pindah ke tab kiri',
     'Switch to Right Tab' => 'Pindah ke tab kanan',
     'Switch Tab' => 'Pindah Tab',
-    'Go to the Top of the File List' => 'Pindah Ke atas dari Daftar File',
-    'Go to the Last File' => 'Pindah File terakhir',
-    'Open File / Enter Directory' => 'Buka File/Masuk ke Direktori',
-    'Edit File' => 'Sunting File',
+    'Go to the Top of the File List' => 'Pindah Ke atas dari Daftar Berkas',
+    'Go to the Last File' => 'Pindah Berkas terakhir',
+    'Open File / Enter Directory' => 'Buka Berkas/Masuk ke Direktori',
+    'Edit File' => 'Sunting Berkas',
     'Go to Parent Directory' => 'Ke Direktori Induk',
-    'Select Current File' => 'Pilih File ini',
-    'Select Bunch of Files' => 'Pilih Bunch of Files',
-    'Add File to the Current Selection' => 'Tambahkan File di Seleksi ini',
-    'Select All Files' => 'Pilih semua file',
+    'Select Current File' => 'Pilih Berkas ini',
+    'Select Bunch of Files' => 'Pilih banyak berkas',
+    'Add File to the Current Selection' => 'Tambahkan Berkas diseleksi ini',
+    'Select All Files' => 'Pilih semua berkas',
     'shortcuts are inspired by magnificent GNU <a href="https://www.midnight-commander.org/">Midnight Commander</a> file manager' =>
         'pintasan terinspirasi oleh magnificent GNU <a href="https://www.midnight-commander.org/">Midnight Commander</a> file manager',
 
@@ -706,7 +706,7 @@ $LANG['id'] = array(
     'Licence Activated' => 'Lisensi telah aktif',
     'Licence Deactivated' => 'Lisensi telah di nonaktifkan',
     'Restrict users so that they cannot use SSH and access only their home directory.' => 'Batasi pengguna agar tidak dapat menggunakan SSH dan hanya dapat mengakses direktori home.',
-    'Browse, copy, edit, view, and retrieve all of your web domain files using fully featured File Manager.' => 'Jelajahi, salin, sunting, lihat dan dapatkan semua file web anda menggunakan File Manager.',
+    'Browse, copy, edit, view, and retrieve all of your web domain files using fully featured File Manager.' => 'Jelajahi, salin, sunting, lihat dan dapatkan semua berkas web anda menggunakan File Manager.',
     'This is a commercial module, you would need to purchace license key to enable it.' => 'Ini modul komersial, anda perlu membayar lisensi untuk mengaktifkannya.',
 
     'Minutes' => 'Permenit',

web/inc/i18n/nl.php
@@ -758,5 +758,5 @@ $LANG['nl'] = array(
 
     'maximum characters length, including prefix' => 'maximaal %s karakters lang, inclusief prefix',
 
-    'Email Credentials' => 'Email Credentials',
+    'Email Credentials' => 'E-mailreferenties',
 );

web/inc/i18n/sr.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Vesta Serbian sr_SR language file
- * Translation base taken from Bosnian translation file and adapted to Serbian (with a lot of modifications)
+ * Translation base taken from Bosnian translation file and adapted to Serbian (with lot of modifications)
  *
  */
 $LANG['sr'] = array(
@@ -559,7 +559,7 @@ $LANG['sr'] = array(
     'MAIL Server' => 'MAIL Server',
     'Antivirus' => 'Antivirus',
     'AntiSpam' => 'AntiSpam',
-    'Use Web Domain SSL Certificate' => 'Use Web Domain SSL Certificate',
+    'Use Web Domain SSL Certificate' => 'Koristi Web Domain SSL sertifikat',
     'Webmail URL' => 'Webmail URL',
     'MySQL Support' => 'MySQL podrška',
     'phpMyAdmin URL' => 'phpMyAdmin URL',

src/rpm/specs/vesta-ioncube.spec
@@ -1,6 +1,6 @@
 Name:           vesta-ioncube
 Version:        0.9.8
-Release:        24
+Release:        26
 Summary:        ionCube Loader
 Group:          System Environment/Base
 License:        "Freely redistributable without restriction"

src/rpm/specs/vesta-nginx.spec
@@ -1,6 +1,6 @@
 Name:           vesta-nginx
 Version:        0.9.8
-Release:        24
+Release:        26
 Summary:        Vesta Control Panel
 Group:          System Environment/Base
 License:        BSD-like

src/rpm/specs/vesta-php.spec
@@ -1,6 +1,6 @@
 Name:           vesta-php
 Version:        0.9.8
-Release:        24
+Release:        26
 Summary:        Vesta Control Panel
 Group:          System Environment/Base
 License:        GPL

src/rpm/specs/vesta-softaculous.spec
@@ -1,6 +1,6 @@
 Name:           vesta-softaculous
 Version:        0.9.8
-Release:        24
+Release:        26
 Summary:        Vesta Control Panel
 Group:          System Environment/Base
 License:        Softaculous License

src/rpm/specs/vesta.spec
@@ -1,6 +1,6 @@
 Name:           vesta
 Version:        0.9.8
-Release:        24
+Release:        26
 Summary:        Vesta Control Panel
 Group:          System Environment/Base
 License:        GPL
@@ -68,13 +68,19 @@ fi
 %config(noreplace) %{_vestadir}/web/css/uploadify.css
 
 %changelog
+* Sat Sep 29 2019 Serghey Rodin <builder@vestacp.com> - 0.9.8-26
+- Let's Encrypt HTTP/2 support
+
+* Thu Aug 15 2019 Serghey Rodin <builder@vestacp.com> - 0.9.8-25
+- Security bugfixes
+- LEv2 idn fix
+
 * Mon Apr 18 2019 Serghey Rodin <builder@vestacp.com> - 0.9.8-24
 - Bugfixes
 - Security patches
 - LEv2
 - Server LE
 - i18n updates
-
 
 * Thu Oct 18 2018 Serghey Rodin <builder@vestacp.com> - 0.9.8-23
 - Security fixes